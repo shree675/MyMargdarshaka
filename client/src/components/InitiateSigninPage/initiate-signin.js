ae0f2298d6249e9fa9853ecda1b984c840257940
shree675 <shreeteshm37@gmail.com>
2021-09-22 21:03:25 +0530

    modified nothing

--- client/src/components/InitiateSigninPage/initiate-signin.js
+++ client/src/components/InitiateSigninPage/initiate-signin.js
@@ -9,7 +9,7 @@ import { useSpring, animated } from "react-spring";
 
 const InitiateSignin = () => {
     const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
-    const trans1 = (x, y) => `translate3d(${x / 16}px,${y / 16}px,0)`;
+    const trans1 = (x, y) => `translate3d(${-x / 16}px,${-y / 16}px,0)`;
     const trans2 = (x, y) => `translate3d(${x / 8}px,${y / 8}px,0)`;
     const trans3 = (x, y) => `translate3d(${x / 6}px,${y / 6}px,0)`;
     const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }));
