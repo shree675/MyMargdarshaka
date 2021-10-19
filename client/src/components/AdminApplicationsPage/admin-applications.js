efe55634a366b25302a4a602ac04a82ba1e998d4
shree675 <shreeteshm37@gmail.com>
2021-10-19 21:07:42 +0530

    completed admin homepage

--- client/src/components/AdminApplicationsPage/admin-applications.js
+++ client/src/components/AdminApplicationsPage/admin-applications.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 import React from "react";
 import AdminNavbar from "../Navbar/admin-navbar";
 import "./admin-applications.css";
@@ -5,222 +7,206 @@ import TextField from "@mui/material/TextField";
 import { IoBan } from "react-icons/io5";
 
 const openApp = [
-  {
-    name: "Arvind",
-    phone: "1234567890",
-    email: "abc@gmail.com",
-    subjects: ["HIN10", "MAT10", "SOC9"],
-    language: "hindi",
-    timeSlot: "morning",
-  },
-  {
-    name: "Raj kumar",
-    phone: "1234567890",
-    email: "abc@gmail.com",
-    subjects: ["HIN10", "MAT10", "SOC9"],
-    language: "hindi",
-    timeSlot: "morning",
-  },
+    {
+        name: "Arvind",
+        phone: "1234567890",
+        email: "abc@gmail.com",
+        subjects: ["HIN10", "MAT10", "SOC9"],
+        language: "hindi",
+        timeSlot: "morning",
+    },
+    {
+        name: "Raj kumar",
+        phone: "1234567890",
+        email: "abc@gmail.com",
+        subjects: ["HIN10", "MAT10", "SOC9"],
+        language: "hindi",
+        timeSlot: "morning",
+    },
 ];
 
 const rejApp = [
-  {
-    name: "Aashrith",
-    phone: "1234567890",
-    email: "abc@gmail.com",
-    subjects: ["HIN10", "MAT10", "SOC9"],
-    language: "hindi",
-    timeSlot: "morning",
-  },
-  {
-    name: "Anil",
-    phone: "1234567890",
-    email: "abc@gmail.com",
-    subjects: ["HIN10", "MAT10", "SOC9"],
-    language: "hindi",
-    timeSlot: "morning",
-  },
+    {
+        name: "Aashrith",
+        phone: "1234567890",
+        email: "abc@gmail.com",
+        subjects: ["HIN10", "MAT10", "SOC9"],
+        language: "hindi",
+        timeSlot: "morning",
+    },
+    {
+        name: "Anil",
+        phone: "1234567890",
+        email: "abc@gmail.com",
+        subjects: ["HIN10", "MAT10", "SOC9"],
+        language: "hindi",
+        timeSlot: "morning",
+    },
 ];
 
 const ApplicationCard = (props) => {
-  const handleApprove = (user) => {
-    console.log("approve ", user.name);
-  };
-  const handleReject = (user) => {
-    console.log("reject ", user.name);
-  };
+    const handleApprove = (user) => {
+        console.log("approve ", user.name);
+    };
+    const handleReject = (user) => {
+        console.log("reject ", user.name);
+    };
 
-  return (
-    <div className="admin-applications-card">
-      <div className="admin-applications-card-row1">
-        <div>{props.app.name}</div>
-        <div>{props.app.phone}</div>
-      </div>
-      <div className="admin-applications-card-row2">
-        <div>Email : {props.app.email}</div>
-        <div>
-          <b>Language : </b>
-          {props.app.language}
-        </div>
-        <div>
-          <b>Time slot : </b>
-          {props.app.timeSlot}
-        </div>
-        <div>
-          <b>Classes : </b>
-          {props.app.subjects.map((sub) => (
-            <div style={{ marginLeft: "2vw" }}>{sub}</div>
-          ))}
-        </div>
-      </div>
+    return (
+        <div className='admin-applications-card'>
+            <div className='admin-applications-card-row1'>
+                <div>{props.app.name}</div>
+                <div>{props.app.phone}</div>
+            </div>
+            <div className='admin-applications-card-row2'>
+                <div>Email : {props.app.email}</div>
+                <div>
+                    <b>Language : </b>
+                    {props.app.language}
+                </div>
+                <div>
+                    <b>Time slot : </b>
+                    {props.app.timeSlot}
+                </div>
+                <div>
+                    <b>Classes : </b>
+                    {props.app.subjects.map((sub) => (
+                        <div style={{ marginLeft: "2vw" }}>{sub}</div>
+                    ))}
+                </div>
+            </div>
 
-      <div className="admin-applications-card-row3">
-        <div
-          className="admin-applications-card-button"
-          onClick={() => {
-            handleApprove(props.app);
-          }}
-          style={props.rej ? { display: "none" } : {}}
-        >
-          APPROVE
+            <div className='admin-applications-card-row3'>
+                <div
+                    className='admin-applications-card-button'
+                    onClick={() => {
+                        handleApprove(props.app);
+                    }}
+                    style={props.rej ? { display: "none" } : {}}
+                >
+                    APPROVE
+                </div>
+                <div
+                    className='admin-applications-card-button'
+                    onClick={() => {
+                        handleReject(props.app);
+                    }}
+                    style={props.rej ? { display: "none" } : {}}
+                >
+                    REJECT
+                </div>
+            </div>
         </div>
-        <div
-          className="admin-applications-card-button"
-          onClick={() => {
-            handleReject(props.app);
-          }}
-          style={props.rej ? { display: "none" } : {}}
-        >
-          REJECT
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 class AdminApplications extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tab: 0,
-      searchText: "",
-      searchResultText: "",
-    };
-    this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
-    this.handleSubmitText = this.handleSubmitText.bind(this);
-    this.handleBanUser = this.handleBanUser.bind(this);
-  }
-
-  handleSearchTextChange(e) {
-    this.setState({ searchText: e.target.value });
-  }
-
-  handleSubmitText(e) {
-    if (e.keyCode === 13) {
-      console.log(this.state.searchText);
-      this.setState({ searchResultText: this.state.searchText });
-      // submit this search text to Backend for Query
+    constructor(props) {
+        super(props);
+        this.state = {
+            tab: 0,
+            searchText: "",
+            searchResultText: "",
+        };
+        this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+        this.handleSubmitText = this.handleSubmitText.bind(this);
+        this.handleBanUser = this.handleBanUser.bind(this);
     }
-  }
 
-  handleBanUser(user) {
-    console.log("ban ", user.name);
-  }
+    handleSearchTextChange(e) {
+        this.setState({ searchText: e.target.value });
+    }
 
-  render() {
-    return (
-      <div>
-        <AdminNavbar />
-        <div className="admin-main">
-          <div className="admin-applications-leftbox">
-            <div className="admin-applications-tab-switcher">
-              <div
-                className="admin-applications-tab-button"
-                style={
-                  this.state.tab == 0
-                    ? { border: "solid 3px red", opacity: 1 }
-                    : {}
-                }
-                onClick={() => {
-                  this.setState({ tab: 0 });
-                }}
-              >
-                OPEN APPLICATIONS
-              </div>
-              <div
-                className="admin-applications-tab-button"
-                style={
-                  this.state.tab == 1
-                    ? { border: "solid 3px red", opacity: 1 }
-                    : {}
-                }
-                onClick={() => {
-                  this.setState({ tab: 1 });
-                }}
-              >
-                REJECTED APPLICATIONS
-              </div>
-            </div>
-            <div
-              style={this.state.tab == 1 ? { display: "none" } : {}}
-              className="admin-applications-applications"
-            >
-              {openApp.map((item) => (
-                <ApplicationCard app={item} />
-              ))}
-            </div>
-            <div
-              style={this.state.tab == 0 ? { display: "none" } : {}}
-              className="admin-applications-applications"
-            >
-              {rejApp.map((item) => (
-                <ApplicationCard app={item} rej={true} />
-              ))}
-            </div>
-          </div>
-          <div className="admin-applications-right-box">
-            <TextField
-              id="outlined-basic"
-              label="Search for User by Name or Phone Number"
-              variant="outlined"
-              value={this.state.searchText}
-              onChange={this.handleSearchTextChange}
-              onKeyUp={this.handleSubmitText}
-            />
-            <div className="admin-applications-search-results">
-              <div>
-                {this.state.searchResultText != ""
-                  ? `Search results for \"${this.state.searchResultText}\"`
-                  : ""}
-              </div>
-              {
-                /* dummy data */
-                openApp.map((user, i) => (
-                  <div
-                    className="admin-applications-search-results-card"
-                    style={{ background: i % 2 == 0 ? "#E8DEE5" : "#F9F6F8" }}
-                  >
-                    <div>
-                      <div>{user.name}</div>
-                      <div>{user.phone}</div>
+    handleSubmitText(e) {
+        if (e.keyCode === 13) {
+            console.log(this.state.searchText);
+            this.setState({ searchResultText: this.state.searchText });
+            // submit this search text to Backend for Query
+        }
+    }
+
+    handleBanUser(user) {
+        console.log("ban ", user.name);
+    }
+
+    render() {
+        return (
+            <div>
+                <AdminNavbar />
+                <div className='admin-main'>
+                    <div className='admin-applications-leftbox'>
+                        <div className='admin-applications-tab-switcher'>
+                            <div
+                                className='admin-applications-tab-button'
+                                style={this.state.tab == 0 ? { border: "solid 3px red", opacity: 1 } : {}}
+                                onClick={() => {
+                                    this.setState({ tab: 0 });
+                                }}
+                            >
+                                OPEN APPLICATIONS
+                            </div>
+                            <div
+                                className='admin-applications-tab-button'
+                                style={this.state.tab == 1 ? { border: "solid 3px red", opacity: 1 } : {}}
+                                onClick={() => {
+                                    this.setState({ tab: 1 });
+                                }}
+                            >
+                                REJECTED APPLICATIONS
+                            </div>
+                        </div>
+                        <div style={this.state.tab == 1 ? { display: "none" } : {}} className='admin-applications-applications'>
+                            {openApp.map((item) => (
+                                <ApplicationCard app={item} />
+                            ))}
+                        </div>
+                        <div style={this.state.tab == 0 ? { display: "none" } : {}} className='admin-applications-applications'>
+                            {rejApp.map((item) => (
+                                <ApplicationCard app={item} rej={true} />
+                            ))}
+                        </div>
                     </div>
-                    <div
-                      onClick={() => {
-                        this.handleBanUser(user);
-                      }}
-                    >
-                      BAN USER
-                      <IoBan style={{ color: "red", marginLeft: "1vw" }} />
+                    <div className='admin-applications-right-box'>
+                        <TextField
+                            id='outlined-basic'
+                            label='Search for User by Name or Phone Number'
+                            variant='outlined'
+                            value={this.state.searchText}
+                            onChange={this.handleSearchTextChange}
+                            onKeyUp={this.handleSubmitText}
+                        />
+                        <div className='admin-applications-search-results'>
+                            <div>
+                                {this.state.searchResultText != "" ? `Search results for \"${this.state.searchResultText}\"` : ""}
+                            </div>
+                            {
+                                /* dummy data */
+                                openApp.map((user, i) => (
+                                    <div
+                                        className='admin-applications-search-results-card'
+                                        style={{ background: i % 2 == 0 ? "#E8DEE5" : "#F9F6F8" }}
+                                    >
+                                        <div>
+                                            <div>{user.name}</div>
+                                            <div>{user.phone}</div>
+                                        </div>
+                                        <div
+                                            onClick={() => {
+                                                this.handleBanUser(user);
+                                            }}
+                                        >
+                                            BAN USER
+                                            <IoBan style={{ color: "red", marginLeft: "1vw" }} />
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
-                  </div>
-                ))
-              }
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default AdminApplications;
--- client/src/components/FeedbackPage/feedback.js
+++ client/src/components/FeedbackPage/feedback.js
@@ -12,149 +12,149 @@ import axios from "axios";
 import firebase from "../../firebase";
 
 const CssTextField = styled(TextField)({
-  "& label.Mui-focused": {
-    color: "#4e0d3a",
-  },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "#4e0d3a",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "gray",
-      border: "none",
-      borderBottom: "2px solid #4e0d3a",
-      borderRadius: "0px",
+    "& label.Mui-focused": {
+        color: "#4e0d3a",
     },
-    "&:hover fieldset": {
-      borderColor: "gray",
+    "& .MuiInput-underline:after": {
+        borderBottomColor: "#4e0d3a",
     },
-    "&.Mui-focused fieldset": {
-      border: "none",
-      borderBottom: "2px solid #4e0d3a",
-      borderRadius: "0px",
+    "& .MuiOutlinedInput-root": {
+        "& fieldset": {
+            borderColor: "gray",
+            border: "none",
+            borderBottom: "2px solid #4e0d3a",
+            borderRadius: "0px",
+        },
+        "&:hover fieldset": {
+            borderColor: "gray",
+        },
+        "&.Mui-focused fieldset": {
+            border: "none",
+            borderBottom: "2px solid #4e0d3a",
+            borderRadius: "0px",
+        },
     },
-  },
 });
 
 const Feedback = () => {
-  const [issueType, setIssueType] = useState(1);
-  const [issue, setIssue] = useState(null);
-  const [subject, setSubject] = useState(null);
-  const [body, setBody] = useState(null);
-  const [curuser, setCuruser] = useState(null);
-  const [phone, setPhone] = useState(null);
+    const [issueType, setIssueType] = useState(1);
+    const [issue, setIssue] = useState(null);
+    const [subject, setSubject] = useState(null);
+    const [body, setBody] = useState(null);
+    const [curuser, setCuruser] = useState(null);
+    const [phone, setPhone] = useState(null);
 
-  useEffect(() => {
-    verify();
-  }, []);
+    useEffect(() => {
+        verify();
+    }, []);
 
-  const verify = async () => {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setCuruser(user.uid);
-        setPhone(user.phoneNumber);
-      } else {
-        window.location = "/initsignin";
-      }
-    });
-  };
+    const verify = async () => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                setCuruser(user.uid);
+                setPhone(user.phoneNumber);
+            } else {
+                window.location = "/init-signin";
+            }
+        });
+    };
 
-  return (
-    <div>
-      <Navbar />
-      <div style={{ height: "35px", backgroundColor: "#720d5d" }}></div>
-      <div className="feedback-content">
-        <div className="feedback-content-left">
-          <img src={feedbackcomp} className="feedback-img"></img>
+    return (
+        <div>
+            <Navbar />
+            <div style={{ height: "35px", backgroundColor: "#720d5d" }}></div>
+            <div className='feedback-content'>
+                <div className='feedback-content-left'>
+                    <img src={feedbackcomp} className='feedback-img'></img>
+                </div>
+                <div className='feedback-content-right'>
+                    <CssTextField
+                        id='subject-input'
+                        label='Subject'
+                        variant='outlined'
+                        fullWidth
+                        size='small'
+                        color='error'
+                        onChange={(e) => {
+                            setSubject(e.target.value);
+                        }}
+                    />
+                    <br></br>
+                    <br></br>
+                    <Select
+                        id='issue-type'
+                        value={issueType}
+                        label='Issue Type'
+                        onChange={(e) => {
+                            setIssueType(e.target.value);
+                            if (e.target.value == 10) {
+                                setIssue("Report Abuse");
+                            } else if (e.target.value == 20) {
+                                setIssue("Platform Issue");
+                            } else if (e.target.value == 30) {
+                                setIssue("Question");
+                            } else if (e.target.value == 40) {
+                                setIssue("Other");
+                            } else {
+                                setIssue(null);
+                            }
+                        }}
+                        fullWidth
+                        size='small'
+                    >
+                        <MenuItem value={1} style={{ color: "gray" }}>
+                            <span style={{ color: "gray" }}>Type of Issue</span>
+                        </MenuItem>
+                        <MenuItem value={10}>Report Abuse</MenuItem>
+                        <MenuItem value={20}>Platform Issue</MenuItem>
+                        <MenuItem value={30}>Question</MenuItem>
+                        <MenuItem value={40}>Other</MenuItem>
+                    </Select>
+                    <br></br>
+                    <br></br>
+                    <CssTextField
+                        id='issue-body'
+                        label='Body of the issue'
+                        variant='outlined'
+                        fullWidth
+                        size='small'
+                        color='error'
+                        multiline
+                        rows={12}
+                        onChange={(e) => {
+                            setBody(e.target.value);
+                        }}
+                    />
+                    <div className='feedback-height'></div>
+                    <button
+                        className='feedback-button'
+                        onClick={() => {
+                            var err = 0;
+                            if (subject == null || issue == null || body == null) {
+                                err = 1;
+                                alert("Please fill all the entries");
+                            }
+                            var feedback = {
+                                phone: phone,
+                                issueSubject: subject,
+                                issueType: issue,
+                                issueBody: body,
+                                timestamp: new Date().toString(),
+                            };
+                            if (!err) {
+                                axios
+                                    .post("/api/feedback/api/submitfeedback", feedback)
+                                    .then((res) => console.log(res))
+                                    .catch((err) => console.error(err));
+                            }
+                        }}
+                    >
+                        SUBMIT
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="feedback-content-right">
-          <CssTextField
-            id="subject-input"
-            label="Subject"
-            variant="outlined"
-            fullWidth
-            size="small"
-            color="error"
-            onChange={(e) => {
-              setSubject(e.target.value);
-            }}
-          />
-          <br></br>
-          <br></br>
-          <Select
-            id="issue-type"
-            value={issueType}
-            label="Issue Type"
-            onChange={(e) => {
-              setIssueType(e.target.value);
-              if (e.target.value == 10) {
-                setIssue("Report Abuse");
-              } else if (e.target.value == 20) {
-                setIssue("Platform Issue");
-              } else if (e.target.value == 30) {
-                setIssue("Question");
-              } else if (e.target.value == 40) {
-                setIssue("Other");
-              } else {
-                setIssue(null);
-              }
-            }}
-            fullWidth
-            size="small"
-          >
-            <MenuItem value={1} style={{ color: "gray" }}>
-              <span style={{ color: "gray" }}>Type of Issue</span>
-            </MenuItem>
-            <MenuItem value={10}>Report Abuse</MenuItem>
-            <MenuItem value={20}>Platform Issue</MenuItem>
-            <MenuItem value={30}>Question</MenuItem>
-            <MenuItem value={40}>Other</MenuItem>
-          </Select>
-          <br></br>
-          <br></br>
-          <CssTextField
-            id="issue-body"
-            label="Body of the issue"
-            variant="outlined"
-            fullWidth
-            size="small"
-            color="error"
-            multiline
-            rows={12}
-            onChange={(e) => {
-              setBody(e.target.value);
-            }}
-          />
-          <div className="feedback-height"></div>
-          <button
-            className="feedback-button"
-            onClick={() => {
-              var err = 0;
-              if (subject == null || issue == null || body == null) {
-                err = 1;
-                alert("Please fill all the entries");
-              }
-              var feedback = {
-                phone: phone,
-                issueSubject: subject,
-                issueType: issue,
-                issueBody: body,
-                timestamp: new Date().toString(),
-              };
-              if (!err) {
-                axios
-                  .post("/api/feedback/api/submitfeedback", feedback)
-                  .then((res) => console.log(res))
-                  .catch((err) => console.error(err));
-              }
-            }}
-          >
-            SUBMIT
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Feedback;
