7f20f3f11581706b16a72a1572a64f066cb97e0a
shree675 <shreeteshm37@gmail.com>
2021-11-03 17:47:30 +0530

    completed chat functionality

--- client/src/components/Chat/chat.js
+++ client/src/components/Chat/chat.js
@@ -1,55 +1,50 @@
 //@ts-check
 
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 // import { Launcher } from "react-chat-window-timestamp-fork";
 import "./chat.css";
 import { Launcher } from "react-chat-window";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import firebase from "../../firebase";
 
-//const firebaseConfig = {
-//apiKey: "AIzaSyDVI09hwundggfP9IrC4iChlVf2xlG4bTc",
-//authDomain: "mymargdarshakaweb.firebaseapp.com",
-//projectId: "mymargdarshakaweb",
-//storageBucket: "mymargdarshakaweb.appspot.com",
-//messagingSenderId: "285220513922",
-//appId: "1:285220513922:web:3f4bab8e8070694fe6ec9b",
-//measurementId: "G-BY7CXNRTWW",
-//};
-
-//if(!firebase.apps.length){
-//var firebaseApp = firebase.initializeApp(firebaseConfig)
-//}else{
-//firebase.app()
-//}
 function isNan(x) {
   return x != x;
 }
 
-function Chat({ collection_name, userType }) {
+function Chat({ collection_name, userType, name }) {
   const firestore = firebase.firestore(firebase.app());
 
-  console.log(collection_name);
+  // console.log(collection_name);
+  // collection_name = "collection0";
 
-  const messagesRef = firestore.collection(
-    !collection_name ? "default" : collection_name
-  );
-  console.log(messagesRef);
+  const messagesRef = firestore.collection(!collection_name ? "default" : collection_name);
+  // console.log(messagesRef);
 
   const query = messagesRef.orderBy("timestamp").limit(25);
-  console.log(query);
+  // console.log(query);
 
   const [messages] = useCollectionData(query, { idField: "id" });
 
+  console.log(userType);
+
   console.log(messages);
 
-  if (messages === undefined || messages === []) {
-    messagesRef.add({
-      author: userType,
-      timestamp: new Date(),
-      text: "Welcome to the chat!",
-    });
-  }
+  // try {
+  //   if (messages === [] && messages !== undefined) {
+  //     messagesRef.add({
+  //       author: userType,
+  //       timestamp: new Date(),
+  //       text: "Welcome to the chat!",
+  //     });
+  //     console.log("messages is []");
+  //   }
+  // } catch (err) {
+  //   console.log(err);
+  // }
+
+  // useEffect(() => {
+
+  // }, [messages]);
 
   const textMessages =
     messages === undefined
@@ -63,18 +58,14 @@ function Chat({ collection_name, userType }) {
           };
         });
 
-  console.log(textMessages);
-
   return (
-    <div className="chat-container">
+    <div className='chat-container'>
       <Launcher
         agentProfile={{
-          teamName: "Chat with <mentor name>",
-          imageUrl:
-            "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png",
+          teamName: `Chat with ${name}`,
+          imageUrl: "",
         }}
         onMessageWasSent={(msg) => {
-          // console.log(msg);
           if (msg.type === "text") {
             messagesRef.add({
               author: userType,
--- client/src/components/LearnerHomePage/card.js
+++ client/src/components/LearnerHomePage/card.js
@@ -3,7 +3,6 @@
 import React, { useEffect } from "react";
 import Chat from "../Chat/chat";
 import "./learner-home.css";
-import firebase from "../../firebase";
 
 const Card = (props) => {
   var showPendingTests = "";
@@ -22,56 +21,47 @@ const Card = (props) => {
   }
 
   return (
-    <div className="container-fluid p-0">
+    <div className='container-fluid p-0'>
       <div
-        className="card text-center m-3"
+        className='card text-center m-3'
         style={{
           backgroundColor: "#5D1049",
           color: "white",
           borderRadius: "20px",
         }}
       >
-        <div className="card-body">
-          <h5 className="card-title mb-3">{props.details["subject"]}</h5>
+        <div className='card-body'>
+          <h5 className='card-title mb-3'>{props.details["subject"]}</h5>
           <div
-            className="rounded-circle mx-auto mb-3"
+            className='rounded-circle mx-auto mb-3'
             style={{
               width: "100px",
               height: "100px",
               backgroundColor: "#ff0000",
             }}
           ></div>
-          <h4 className="card-text mb-3">{props.details["name"]}</h4>
-          <p className="card-text">Class {props.details["Class"]}</p>
-          <p className="card-text">{props.details["subject"]}</p>
-          <p className={"card-text  " + showDetails}>
-            {props.details["email"]}
-          </p>
-          <p className={"card-text  " + showDetails}>
-            {props.details["phone"]}
-          </p>
+          <h4 className='card-text mb-3'>{props.details["name"]}</h4>
+          <p className='card-text'>Class {props.details["Class"]}</p>
+          <p className='card-text'>{props.details["subject"]}</p>
+          <p className={"card-text  " + showDetails}>{props.details["email"]}</p>
+          <p className={"card-text  " + showDetails}>{props.details["phone"]}</p>
 
-          <a
-            href="#"
-            className={"btn btn-warning text-white " + showPendingTests}
-          >
+          <a href='#' className={"btn btn-warning text-white " + showPendingTests}>
             <b>YOU HAVE PENDING TESTS</b>
           </a>
 
-          <a
-            href="#"
-            className={"btn btn-warning text-white " + showConsentButton}
-          >
+          <a href='#' className={"btn btn-warning text-white " + showConsentButton}>
             <b>CLICK HERE TO SHARE YOUR NUMBER</b>
           </a>
-          <div className="card-chat">
-            <Chat
-              collection_name={
-                props.details["learner_id"] + props.details["mentor_id"]
-              }
-              userType={props.details.userType}
-            />
-          </div>
+          {props.details["learner_id"] === undefined || props.details["mentor_id"] === undefined ? null : (
+            <div className='card-chat'>
+              <Chat
+                collection_name={props.details["learner_id"] + props.details["mentor_id"]}
+                userType={props.details.userType}
+                name={props.details["name"]}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
