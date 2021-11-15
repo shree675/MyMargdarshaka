28bc285735a773c04876f58448ecd6870c5a2f60
shree675 <shreeteshm37@gmail.com>
2021-11-15 16:26:32 +0530

    removed anchor tag css

--- client/src/components/FeedbackButton/Feedback.jsx
+++ client/src/components/FeedbackButton/Feedback.jsx
@@ -3,7 +3,11 @@ import "./Feedback.css";
 
 // a button that takes to the given url
 const Feedback = ({ url }) => {
-  return <a href={url}>Feedback</a>;
+  return (
+    <a href={url} className='a'>
+      Feedback
+    </a>
+  );
 };
 
 export default Feedback;
