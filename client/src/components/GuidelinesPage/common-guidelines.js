626f12f8f30e2465f5a87b1f0cd0f9f9ac418f65
Debeshee <debesheedas@gmail.com>
2021-11-15 18:29:57 +0530

    minor changes to common guidelines

--- client/src/components/GuidelinesPage/common-guidelines.js
+++ client/src/components/GuidelinesPage/common-guidelines.js
@@ -1,12 +1,16 @@
 // @ts-check
 import React from "react";
 // @ts-ignore
-/* import lola from "../../assets/virtual_mentor_antennae.gif"; */
 import lola from "../../assets/lola_with_sparkle_trail.gif";
-import ChatBubble from "react-chat-bubble";
 import "./common-guidelines.css";
+import Chatbot from "react-chatbot-kit";
+import config from "../../chatbot/config";
+import MessageParser from "../../chatbot/MessageParser";
+import ActionProvider from "../../chatbot/ActionProviderCommon";
+import "./common-guidelines.css";
+import "react-chatbot-kit/build/main.css";
 
-const CommonGuidelines = () => {
+/* const CommonGuidelines = () => {
   return (
     <div className='body'>
       <div className='container'>
@@ -16,19 +20,17 @@ const CommonGuidelines = () => {
               <div>Hello there! I'm Lola! How can I help you?</div>
                 
                 </div>
-            {/* <ChatBubble
-              className={"bubble"}
-              messages={[
-                {
-                  image: "../../assets/lock.svg",
-                  text: "Hello! Good Morning!",
-                },
-              ]}
-            /> */}
           </div>
           <div className= 'row'>
             <div className= 'lola-row'>
-            <img src={lola} /* style={{ width: "596px", height: "842px" }} */ alt='' />
+            <img className= 'col' src={lola}  alt='' />
+            <div className="col" style={{ backgroundColor: "white" }}>
+              <Chatbot
+                config={config}
+                actionProvider={ActionProvider}
+                messageParser={MessageParser}
+              />
+            </div>
             </div>
             
           </div>
@@ -38,4 +40,36 @@ const CommonGuidelines = () => {
   );
 };
 
+export default CommonGuidelines; */
+
+const CommonGuidelines = () => {
+  return (
+    <div className="body">
+      <div className="container">
+        <div className="col">
+          <div className="row">
+            <div className="bubble">Hello there! I'm Lola! How can I help you?</div>
+          </div>
+          <div className="row">
+            <img
+              className="col"
+              src={lola}
+              /* style={{ width: "596px", height: "842px" }} */
+              alt=""
+            />
+            <div className="col" style={{ backgroundColor: "white" }}>
+              <Chatbot
+                config={config}
+                actionProvider={ActionProvider}
+                messageParser={MessageParser}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default CommonGuidelines;
+
