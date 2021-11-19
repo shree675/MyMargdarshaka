c2edc10a5608d7bc34718e2111532c1744924f11
Debeshee <debesheedas@gmail.com>
2021-11-19 20:11:02 +0530

    fact api and more message handling added to message parser

--- client/src/chatbot/MessageParser.js
+++ client/src/chatbot/MessageParser.js
@@ -1,9 +1,12 @@
+import messages from "./messages";
+import axios from "axios";
+
 class MessageParser {
   constructor(actionProvider, state) {
     this.actionProvider = actionProvider;
     this.state = state;
   }
-  
+
 
   parse(message) {
     console.log(message);
@@ -14,23 +17,47 @@ class MessageParser {
     }
     else if ((lowercase.includes("I love you")|| lowercase.includes("like you")))
     {
-      this.actionProvider.handleSingleOption("I love me too! I'm Lola! How can I help you? ", "options");
+      this.actionProvider.handleSingleOption("I love me too! I'm Lola and I like forests! How can I help you? ", "options");
     }
-    else if ((lowercase.includes("how are you")|| lowercase.includes("you")))
+    else if ((lowercase.includes("how are you")|| lowercase.includes("you")|| lowercase.includes("whats up")|| lowercase.includes("sup")|| lowercase.includes("wyd")))
     {
       this.actionProvider.handleSingleOption("I'm doing well, thank you! My name is Lola and my preferred pronouns are they/them. How can I help you today?", "options");
     }
-    else if((lowercase.includes("joke")|| lowercase.includes("laugh")))
+    else if ((lowercase.includes("robot")|| lowercase.includes("human")))
     {
-      let joke = ""
+      this.actionProvider.handleSingleOption("Heyyy, I’m Lola, not a robot! I can help you with the following!", "options");
+    }
+    else if ((lowercase.includes("margdarshaka")|| lowercase.includes("how to")))
+    {
+      this.actionProvider.handleSingleOption("Hmmm...do you want help with using My Margdarshaka? Maybe try the following", "gettingStarted");
+    }
+    else if((lowercase.includes("joke")|| lowercase.includes("laugh")|| lowercase.includes("funny")|| lowercase.includes("pun")|| lowercase.includes("fun")))
+    {
       fetch("https://sv443.net/jokeapi/v2/joke/Pun?safe-mode&type=single")
       .then((res) => res.json())
-      .then((data) => {console.log(data.joke);
+      .then((data) => {/* console.log(data.joke) */;
         this.actionProvider.handleSingleOption("Here's something that might make you laugh. \n\n"+ data.joke, "options")})
+    }
+    else if((lowercase.includes("something")|| lowercase.includes("fact")|| lowercase.includes("anything")|| lowercase.includes("random")|| lowercase.includes("bored")))
+    {
+      axios.get('http://numbersapi.com/random/trivia')
+      .then(response => {console.log(response.data); this.actionProvider.handleSingleOption("Here's a fun fact for you! \n\n"+ response.data, "options")})
+      .catch(error => console.log(error));
+    }
+    else if((lowercase.includes("?")|| lowercase.includes("how")|| lowercase.includes("what")|| lowercase.includes("when")|| lowercase.includes("why") || lowercase.includes("where")))
+    {
+      let i = (Math.floor(Math.random()))%(messages.questions.length)
+      let message = messages.questions[i];
+      //console.log(message)
+      this.actionProvider.handleSingleOption(message, "options");
     }
     else
     {
-      this.actionProvider.handleSingleOption("I'm sorry, I didn't quite understand that. Please choose on of the options I can help you with. ", "options");     
+      let i = (Math.floor(Math.random()))%(messages.default.length)
+      let message = messages.default[i];
+      //console.log(message)
+      this.actionProvider.handleSingleOption(message, "options");
+      //this.actionProvider.handleSingleOption("I'm sorry, I didn't quite understand that. Please choose on of the options I can help you with. ", "options");     
     }
   }
 }
---
+++ client/src/chatbot/messages.js
@@ -0,0 +1,22 @@
+const messages = {
+    questions: ["Hmmm...I’m afraid I don’t know how to answer that. Maybe I can help you with one of these?",
+                "I wish I could help you with that but unfortunately I didn’t understand. Can I help you with one of these instead?",
+                "That is an interesting question! I’m sorry I don’t know the answer though. What else can I help you with?",
+                "I like forests, oceans and cupcakes. I wonder what you like. By the way, can I help you with any of the following?",
+                "Let’s come back to this some other time. Now, we can talk about My Margdarshaka!"
+            
+],
+    default: ["I’m sorry I didn’t quite understand. Maybe I could try to help you with one of the following?",
+                "Hmmm...interesting. Let’s change the topic. Do you want help with using My Margdarshaka?",
+                "I didn’t know that. Thank you for telling me. Shall we talk about learning online now?",
+                "Cool! By the way, isn’t the weather amazing today? ",
+                "That’s an interesting thought! Let’s put a pin on it and come back to it later.",
+                "Sounds cool! Why don’t we talk about learning online on My Margdarshaka?",
+                "I like the idea. Let’s circle back to this later. In the meantime, can I help you with any of the following?",
+                "Hmmm...that’s interesting. By the way, I’m not sure if I introduced myself, but you can call me Lola. I prefer the pronouns they/them. What about you?",
+                "Cool! Thank you for telling me about that. "
+            
+            ]
+}
+
+export default messages;
