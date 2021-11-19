ec7b9ea5d20b1a7087ebd34e23a840d73417b8ea
Debeshee <debesheedas@gmail.com>
2021-11-19 17:17:07 +0530

    joke api added to lola

--- client/src/chatbot/MessageParser.js
+++ client/src/chatbot/MessageParser.js
@@ -3,6 +3,7 @@ class MessageParser {
     this.actionProvider = actionProvider;
     this.state = state;
   }
+  
 
   parse(message) {
     console.log(message);
@@ -11,7 +12,7 @@ class MessageParser {
     if (lowercase.includes("hello")|| lowercase.includes("hi") || lowercase.includes("hey")) {
       this.actionProvider.handleSingleOption("Hello, my name is Lola! How can I help you? ", "options");
     }
-    else if ((lowercase.includes("I love you")|| lowercase.includes("I like you")))
+    else if ((lowercase.includes("I love you")|| lowercase.includes("like you")))
     {
       this.actionProvider.handleSingleOption("I love me too! I'm Lola! How can I help you? ", "options");
     }
@@ -19,9 +20,17 @@ class MessageParser {
     {
       this.actionProvider.handleSingleOption("I'm doing well, thank you! My name is Lola and my preferred pronouns are they/them. How can I help you today?", "options");
     }
+    else if((lowercase.includes("joke")|| lowercase.includes("laugh")))
+    {
+      let joke = ""
+      fetch("https://sv443.net/jokeapi/v2/joke/Pun?safe-mode&type=single")
+      .then((res) => res.json())
+      .then((data) => {console.log(data.joke);
+        this.actionProvider.handleSingleOption("Here's something that might make you laugh. \n\n"+ data.joke, "options")})
+    }
     else
     {
-      this.actionProvider.handleSingleOption("I'm sorry, I didn't quite understand that. Please choose on of the options I can help you with. ", "options");
+      this.actionProvider.handleSingleOption("I'm sorry, I didn't quite understand that. Please choose on of the options I can help you with. ", "options");     
     }
   }
 }
